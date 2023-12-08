6b140c095c056b8eda475c289f44b94229345e2c
rodrigo0599 <137252960+rodrigo0599@users.noreply.github.com>
2023-12-08 01:46:23 -0500

    Reinstall node modules

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-const express = require("express");
+import express from "express";
 import path from "path";
 import db from "./config/connection";
 import { ApolloServer } from "@apollo/server";
 import { typeDefs, resolvers } from "./schemas";
 import { authMiddleware } from "./utils/auth";
-import { expressMiddleware } from "@apollo/server/express4";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -21,7 +20,7 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-  app.use("/graphql", expressMiddleware(server, { context: authMiddleware }));
+  server.applyMiddleware({ app, path: "/graphql" });
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
